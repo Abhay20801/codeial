ef2bbec8576d6658c1f14eb8320ccd786a162e96
Abhay20801 <102847697+Abhay20801@users.noreply.github.com>
2023-06-19 19:59:49 +0530

    Setup View engine and home page rendered

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000;
 
    // Use Express route
    app.use('/',require('./routes/index'));
+// Set up the view engine
+   app.set('view engine','ejs');
+    app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
